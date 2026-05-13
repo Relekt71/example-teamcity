ebd4bf32838b43d707f8ab5189b528fef5d20f8e
relekt <relekt@TeamCity>
2026-05-13 10:41:13 +0000

    Synchronization with own VCS root is enabled (TeamCity change in 'netology_hw' project)

---
+++ .teamcity/settings.kts
@@ -0,0 +1,65 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2026.1"
+
+project {
+
+    buildType(Build)
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    artifactRules = "target/*.jar => artifacts"
+
+    params {
+        param("env.MAVEN_GOAL", "clean test")
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            name = "Set Maven goal based on branch"
+            id = "Set_Maven_goal_based_on_branch"
+            scriptContent = """
+                if [ "%teamcity.build.branch%" = "master" ]; then
+                  echo "##teamcity[setParameter name='env.MAVEN_GOAL' value='clean deploy']"
+                else
+                  echo "##teamcity[setParameter name='env.MAVEN_GOAL' value='clean test']"
+                fi
+            """.trimIndent()
+        }
+        maven {
+            id = "Maven2"
+            goals = "%env.MAVEN_GOAL%"
+            userSettingsSelection = "settings.xml"
+        }
+    }
+})
